2b5a66b30b07240f5557bc0c59aa8ce866b8a4d4
Natalya Myunster <natalyamyunster@yahoo.com>
2020-08-10 11:56:53 +0200

    added uuid ( yarn add uuidv4)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,16 +2,17 @@ import React, {useState} from 'react';
 import './App.module.css';
 import {FormCreateTask} from "../FormCreateTask";
 import {Board} from "../Board";
+import {v4 as uuid} from 'uuid';
 
 const initialTasks = [
     {
-        id: 1,
+        id: uuid(),
         title: 'First Task',
         priority: 2,
         status: 'todo'
     },
     {
-        id: 2,
+        id: uuid(),
         title: 'Second Task',
         priority: 3,
         status: 'review'
