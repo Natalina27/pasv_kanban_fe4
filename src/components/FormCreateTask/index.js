930c9295d7863b84205b4e6de0e1559a60cf89f2
Natalya Myunster <natalyamyunster@yahoo.com>
2020-08-17 16:40:51 +0200

    added button for open modal and function for close Modal

--- src/components/FormCreateTask/index.js
+++ src/components/FormCreateTask/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import {ModalFooter, Button, ModalBody, Modal, ModalHeader} from "reactstrap";
+import React, {useState} from 'react';
+import { Input, ModalFooter, Button, ModalBody, Modal, ModalHeader, Label} from 'reactstrap';
 
 export const FormCreateTask = () => {
+    const [isModalOpen, setModelOpen] = useState(false);
     return (
-        <Modal isOpen={true}>
-            <ModalHeader > Add new task </ModalHeader>
-            <ModalBody>
-            </ModalBody>
-            <ModalFooter>
-                <Button color="primary" >Add</Button>{' '}
-                <Button color="secondary">Cancel</Button>
-            </ModalFooter>
-        </Modal>
+        <>
+            <Button onClick={()=>setModelOpen(true)}>Add new task</Button>
+            <Modal isOpen={isModalOpen}>
+                <ModalHeader> Add new task </ModalHeader>
+                <ModalBody>
+                    <Label>New Title</Label>
+                    <Input/>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary">Add</Button>{' '}
+                    <Button
+                        color="secondary"
+                        onClick={()=>setModelOpen(false)}>
+                        Cancel
+                    </Button>
+                </ModalFooter>
+            </Modal>
+        </>
     );
 };
